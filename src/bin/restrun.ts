9b0c3319aa15223fb1708dcf897139fe788ea688
tomaswyz <jackywyz@126.com>
2016-08-26 23:57:21 +0800

    feat(*): add the oauth2 test

--- src/bin/restrun.ts
+++ src/bin/restrun.ts
@@ -1,9 +1,10 @@
  
 import * as tsRouter from 'ts-router';
 import * as Koa from 'koa';
-import * as oauthserver from "oauth2-server";
- 
+import * as mem from '../lib/oauth2';
+let oauthServer = require('oauth2-server');
 const app = new Koa();
+const oamodel = new mem.InMemoryCache();
 const router = new tsRouter. Router();
 
 
@@ -16,13 +17,21 @@ constructor(name:string,password:string) {
    this.password = password
 }
 }
-@tsRouter.Path('/test')
+@tsRouter.Path('/:version')
 class TestController extends tsRouter.Controller {
     @tsRouter.PathParam('v1') private v1:string;
     private v2:string;
     constructor() {
         super();
         this.v1 = this.v1 + this.v1;
+    }
+
+    @tsRouter.Path('/grant')
+    @tsRouter.POST
+    @tsRouter.Consume(tsRouter.MediaType.JSON)
+    @tsRouter.Produce(tsRouter.MediaType.JSON)
+    async grant(@tsRouter.Body body:Object):Promise<tsRouter.Response> {
+        return tsRouter.Response.status(200).body({body}).build();
     }
 
     @tsRouter.Path('/:v1/:v2')
@@ -35,18 +44,33 @@ class TestController extends tsRouter.Controller {
     }
 
     @tsRouter.Before
-    async before(@tsRouter.PathParam('v2') v2:string) {
+    
+    
+    async before(@tsRouter.PathParam('version') v2:string,@tsRouter.AppContext context:tsRouter.Context) {
         this.v2 = v2;
     }
+
+    
 }
  
 router.use(TestController);
-let oauth = oauthserver({
-  model: {},
+let oauth = oauthServer({
+  model: oamodel,
   grants: ['password'],
   debug: true
 });
+
+
+
 app.use(router.routes());
+
+/*
+app.use(oauth.authorise())
+app.use( async (ctx, next) => {
+   this.body = "hello"
+  await next();
+});
+*/
 app.listen(3000);
  
 console.log('started at http://localhost:3000');
--- src/lib/oauth2.ts
+++ src/lib/oauth2.ts
@@ -1,20 +1,86 @@
-import * as oauthserver from "oauth2-server";
-let model = {
-  // We support generators.
-  getAccessToken: function() {
-          return { user: {} }
-  },
 
-  // Or, calling a node-style callback.
-  getClient: function() {
-          return { grants: ['authorization_code'], redirectUris: ['http://example.com/cb'] };
-  },
-  saveAuthorizationCode: function() {
-          return { authorizationCode: 123 };
-  },
+export class InMemoryCache {
+  clients :{ [key:string] : any}[]
+  tokens:{ [key:string] : string}[]
+  users:{ [key:string] : string}[]
+constructor(){
+  this.clients = [{ clientId : 'thom', clientSecret : 'nightworld', 'redirectUris' : [''] }];
+  this.tokens = [];
+  this.users = [{ id : '123', username: 'thomseddon', password: 'nightworld' }];
+}
 
-  
+/**
+ * Dump the cache.
+ */
+
+dump() {
+  console.log('clients', this.clients);
+  console.log('tokens', this.tokens);
+  console.log('users', this.users);
 };
 
-let options = { model: model,grants:['password'] };
-let oauth = oauthserver(options);
+/*
+ * Get access token.
+ */
+
+ async getAccessToken (bearerToken:string) {
+  var tokens = this.tokens.filter(function(token) {
+    return token [ 'accessToken' ] === bearerToken;
+  });
+
+  return tokens.length ? tokens[0] : false;
+};
+
+
+/**
+ * Get refresh token.
+ */
+
+async getRefreshToken(bearerToken:string) {
+  let tokens = this.tokens.filter(function(token) {
+    return token [ 'refreshToken' ] === bearerToken;
+  });
+
+  return tokens.length ? tokens[0] : false;
+};
+
+
+/**
+ * Get client.
+ */
+
+async getClient(clientId:string, clientSecret:string) {
+  let clients = this.clients.filter(function(client) {
+    return client [ 'clientId' ] === clientId && client [ 'clientSecret' ] === clientSecret;
+  });
+
+  return clients.length ? clients[0] : false;
+};
+
+/**
+ * Save token.
+ */
+
+async saveToken (token:any, client:any, user:any) {
+  this.tokens.push({
+    accessToken: token['accessToken'],
+    accessTokenExpiresAt: token['accessTokenExpiresAt'],
+    clientId: client['clientId'],
+    refreshToken: token [ 'refreshToken' ],
+    refreshTokenExpiresAt: token [ 'refreshTokenExpiresAt' ],
+    userId: user [ 'id' ]
+  });
+};
+
+/*
+ * Get user.
+ */
+
+async getUser (username:string, password:string) {
+  let users = this.users.filter(function(user) {
+    return user['username'] === username && user [ 'password' ] === password;
+  });
+
+  return users.length ? users[0] : false;
+};
+}
